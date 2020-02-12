51d6fb969ff361d9d4e27af400055692c50bc4c8
Sahil Saini <COMP2101sahil@yourdomain>
2020-02-12 17:41:03 -0500

    lab 3 done

---
+++ bash/passwordguesser.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# This script demonstrates testing to see if 2 strings are the same
+# it also demonstrates using conditional execution
+
+# TASK 1: Improve it by asking the user for a password guess instead of using inline literal data (i.e. TestString)
+# TASK 2: Improve it by rewriting it to use the if command
+#         The if command should test if they got it right, in which case it should tell them they got it right
+#         The if command should tell them they got it wrong
+# TASK 3: Improve it by giving them 5 tries to get it right before failing
+#           *** Do not use the exit command, and do not use a loop (e.g. while or for)
+
+
+
+
+echo -n "guess the user password: "
+password="PaSSw0rd"
+read TestString
+if [ $TestString = $password ];then
+  echo " It's Correct"
+  else
+    echo "Sorry, it's Incorrect"
+    echo "try again"
+    read TestString
+    if [ $TestString = $password ];then
+      echo "It's Correct"
+      else
+        echo "Sorry, it's Incorrect"
+        echo "try again"
+        read TestString
+        if [ $TestString = $password ];then
+          echo "It's Correct"
+          else
+            echo "Sorry, it's Incorrect"
+            echo "try again"
+            read TestString
+            if [ $TestString = $password ];then
+              echo "It's Correct"
+              else
+                echo "Sorry, it's Incorrect"
+                echo "try again"
+                read TestString
+                if [ $TestString = $password ];then
+                  echo "It's Correct"
+                  else
+                    echo "Sorry, it's Incorrect"
+                    echo "try again"
+                  fi
+              fi
+          fi
+      fi
+  fi
